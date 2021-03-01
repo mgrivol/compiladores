c269c117d7fe0bf077a000ebc871e43eef7c5a1f
MarcoGrivol <mbgrivol@gmail.com>
2021-03-01 18:30:37 -0300

    trabalho 1 lexemas

--- Trabalho1/src/main/antlr4/br/ufscar/dc/compiladores/trabalho1/LALexer.g4
+++ Trabalho1/src/main/antlr4/br/ufscar/dc/compiladores/trabalho1/LALexer.g4
@@ -1,15 +1,70 @@
 lexer grammar LALexer;
 
-PALAVRA_CHAVE   : 
-      ALG | 'leia'
-    ;
+PALAVRA_CHAVE 
+: 
+    ( 'algoritmo' | 'declare'   | 'fim_algoritmo' |
+      'literal'   | 'inteiro'   | 'leia'          |
+      'escreva'   | 'constante' | 'logico'        |
+      'falso'     | 'entao'     | 'enquanto'      |
+      'nao'       | 'e'         | 'verdadeiro'    |
+      'se'        | 'faca'      | 'fim_enquanto'  |
+      'fim_se'    | 'tipo'      | 'real'          |
+      'var'       | 'fim_registro' | 'fim_procedimento'
+    )   
+;
 
-ALG     :
-    'algoritmo'
-    ;
+IDENT 
+: 
+    ('a'..'z'|'A'..'Z') ('a'..'z'|'A'..'Z'|'0'..'9')*
+;
 
-IDENT   : 
-        ('a'..'z'|'A'..'Z') ('a'..'z'|'A'..'Z'|'0'..'9')*
-    ;
+NUM_INT 
+:
+    ( '0'..'9' )+
+;
 
+CADEIA 
+:
+    '"' (CADEIA | .)*? '"'
+;
 
+CADEIA_ERRO
+:
+    '"' (CADEIA | .)*?
+;
+
+OPERADORES 
+:
+    ( '='  | '+'  | '-'  |
+      '<=' | '>=' | '<-' |
+      '<'  | '>'  | '/' 
+    )
+;
+
+DELIM 
+:
+    ( ':' | ',' | '(' | 
+      ')' | '[' | ']' | 
+      '_' | '.' 
+    )
+;
+
+WS 
+: 
+    ( ' ' | '\t' | '\r' | '\n' ) -> skip
+;
+
+COMENTARIO_ERRO
+:
+    '{' ~('}')* '\n'
+;
+
+COMENTARIO 
+:
+    '{' ~('}' | '\n')* '}' -> skip
+;
+
+ERRO
+:
+    .
+;
--- Trabalho1/src/main/java/br/ufscar/dc/compiladores/trabalho1/Principal.java
+++ Trabalho1/src/main/java/br/ufscar/dc/compiladores/trabalho1/Principal.java
@@ -1,25 +1,62 @@
 package br.ufscar.dc.compiladores.trabalho1;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
 
 public class Principal {
     public static void main(String args[]) throws IOException {
+        // Tente criar um arquivo parar a saida
+        PrintWriter writer = new PrintWriter(args[1]);
+   
         CharStream cs = CharStreams.fromFileName(args[0]);
         LALexer lex = new LALexer(cs);
         
-        System.out.println("Iniciando Analise Lexica: ");
-        
         Token t = null;
-        while ((t=lex.nextToken()).getType() != Token.EOF) {
-            System.out.println("<" +
-                               "\'" + t.getText() + "\'" +
-                               "," + LALexer.VOCABULARY.getDisplayName(t.getType()) + 
-                               ">");
-        }
+        boolean erro = false;
+        String buffer = new String();
         
-        System.out.println("Analise Lexica Terminada!");
+        while ((t=lex.nextToken()).getType() != Token.EOF && !erro) {
+            // padrao identifica a CLASSE do token
+            String padrao = LALexer.VOCABULARY.getSymbolicName(t.getType());
+
+            if (padrao.equals("PALAVRA_CHAVE") || padrao.equals("DELIM") || 
+                padrao.equals("OPERADORES")) {
+                // Se o padrao do token for uma PALAVRA_CHAVE ou DELIM
+                // System.out.println("<\'" + t.getText() + "\',\'" + t.getText() + "\'>");
+                buffer += "<\'" + t.getText() + "\',\'" + t.getText() + "\'>\r\n";
+            }
+            else if (padrao.equals("IDENT") || padrao.equals("CADEIA") ||
+                     padrao.equals("NUM_INT") ) {
+                // Se o padrao do token for um IDENT ou CADEIA ou NUM_INT
+                // System.out.println("<\'" + t.getText() + "\'," +  padrao + ">");
+                buffer += "<\'" + t.getText() + "\'," +  padrao + ">\r\n";
+            }
+            else if (padrao.equals("ERRO") || padrao.equals("COMENTARIO_ERRO")) {
+                if (padrao.equals("COMENTARIO_ERRO")) {
+                    buffer += "Linha " + t.getLine() + ": " +
+                              "comentario nao fechado\r\n";
+                }
+                else if (padrao.equals("CADEIA_ERRO")) {
+                    buffer += "Linha " + t.getLine() + ": " +
+                              "cadeia literal nao fechada\r\n";
+                }
+                else {
+                    buffer += "Linha " + t.getLine() + ": " + t.getText() + 
+                              " - simbolo nao identificado\r\n";
+                }
+                erro = true;
+            }
+            else {
+                System.out.println("ESTRANHO" + padrao + "   " + t.getText());
+            }
+            
+            writer.print(buffer);
+            buffer = "";
+        }
+        writer.close();
     }
 }
